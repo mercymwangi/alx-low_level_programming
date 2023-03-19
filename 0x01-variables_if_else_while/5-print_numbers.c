96fb18babe30cb862fb42a2fb9ac2b2a9b95386e
mercymwangi <mercien.m@gmail.com>
2023-03-19 09:24:45 -0700

    base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Print single digits
+ * digits of base 10
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char n;
+
+	n = '0';
+		for (n = 0; n <= 10;)
+			putchar(n);
+		n++;
+	putchar('\n');
+	return (0);
+}
