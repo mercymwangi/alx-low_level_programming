a473eba46e92eb9c718dbbfcbcae41ff8209537e
mercymwangi <mercien.m@gmail.com>
2023-03-19 12:15:19 -0700

    reverse alph

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - print numbers in reverse
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char c;
+
+	c = 'z';
+
+		while
+			(c >= 'a') {
+				putchar('c');
+				c--;
+			}
+		putchar('\n');
+		return (0);
+}
