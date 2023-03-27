1ddcbe6ebc9691107789c6ac7ef95f42d39e9090
mercymwangi <mercien.m@gmail.com>
2023-03-27 13:29:06 -0700

    I do not fear computers. I fear the lack of them

---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character to std output
+ * @c: character to print
+ * Return: 0
+ */
+int _putchar(char c)
+{
+	return(c);
+}
