080a97b49eda7a2a5ad78f03cb1b6bad498a45c4
mercymwangi <mercien.m@gmail.com>
2023-03-29 05:06:24 -0700

    strcat

---
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _putchar - prints c character into std output
+ * @c: The character to print
+ *
+ * Return: 0
+ */
+int _putchar(char c)
+{
+	return (c);
+}
