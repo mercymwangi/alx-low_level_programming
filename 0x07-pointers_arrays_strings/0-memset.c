c3f4929e2d3c4d45a632b36dabe7e6ef664609c6
mercymwangi <mercien.m@gmail.com>
2023-04-03 16:10:12 -0700

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memset - fills memory with a constant byte
+ * @s: Starting address of the memory to be filled
+ * @b: The disired value
+ * @n: Number of bytes to be changed
+ * Return: chaged array with new value for n bytes
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+
+	for (; n > 0; i++)
+	{
+		s[i] = b;
+		n--;
+	}
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,14 @@
+#include "man.h"
+#include <unistd.h>
+
+/**
+ * _putchar - prints c in std ouput
+ * @c: The character to be printed
+ *
+ * Return: 1 on success
+ * on error, return -1 
+ */
+int _putchar(char c)
+{
+	return (c);
+}
