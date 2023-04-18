1d9f1d47dc38db8d67b5b3768cf357fa2529e660
mercymwangi <mercien.m@gmail.com>
2023-04-17 18:15:20 -0700

    object-like macro

---
+++ 0x0D-preprocessor/_putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * _putchar - prints character to std output
+ * @c: The character
+ * Return: c
+ */
+int _putchar(char c)
+{
+	return (c);
+}
