410924cbbd9c2cf3c6f3cfba49695ff862ef8084
mercymwangi <mercien.m@gmail.com>
2023-04-19 14:15:40 -0700

    print name

---
+++ 0x0F-function_pointers/_putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * _putchar - prints character to std output
+ * @c: the character
+ * Return: c
+ */
+int _putchar(char c)
+{
+	return (c);
+}
