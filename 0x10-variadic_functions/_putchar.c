a90bced61cc3694cb182db3840ea68f1237ad0e6
mercymwangi <mercien.m@gmail.com>
2023-04-20 17:24:26 -0700

    sum them all

---
+++ 0x10-variadic_functions/_putchar.c
@@ -0,0 +1,12 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+
+/**
+ * _putchar - prints character to std output
+ * @c: the character
+ * Return: c
+ */
+int _putchar(char c)
+{
+	return (c);
+}
