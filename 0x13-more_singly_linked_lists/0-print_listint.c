7af92b1efb1e7b416a913a4a8af576fc9485a91a
mercymwangi <mercien.m@gmail.com>
2023-05-02 10:03:34 -0700

    print listint

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_listint - prints all elements of listint_t list
+ * @h: prints linked list of type listint_t
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t num = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		num++;
+		h = h->next;
+	}
+	return (num);
+}
---
+++ 0x13-more_singly_linked_lists/_putchar.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * _putchar - prints character into std output
+ * @c: the character
+ * Return: c
+ */
+int _putchar(char c)
+{
+	return (c);
+}
