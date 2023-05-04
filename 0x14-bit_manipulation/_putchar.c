4527ec0c6ac72695696ba1c222c084ec251c3ded
mercymwangi <mercien.m@gmail.com>
2023-05-04 10:33:06 -0700

    binary to uint

---
+++ 0x14-bit_manipulation/_putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+ * _putchar - prints character ito std output
+ * @c: The character
+ * Return: c
+ */
+int _putchar(char c)
+{
+	return (c);
+}
